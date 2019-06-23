36bfacff1ccbb8b8e59822d9b44b847e801365d8
ayungavis <ayungavis@gmail.com>
2019-06-24 02:42:55 +0700

    Add jwt authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('jwt.auth')->get('users', function(Request $request) {
+    return auth()->user();
+});
+
+Route::post('user/register', 'APIRegisterController@register');
+Route::post('user/login', 'APILoginController@login');
